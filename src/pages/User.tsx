6ddb09943e5c05965f7b434de2c4c1a5b9bed6c6
danielsheetrit <danielshitrit022@gmail.com>
2023-07-31 21:08:49 +0300

    removing useless useRef

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { Button, Stack, Typography } from "@mui/material";
 import { IQuestion } from "../interfaces/IQuestion";
 
